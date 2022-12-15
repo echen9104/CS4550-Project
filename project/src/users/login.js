f452570a0e122befdc098b7ae8e7584755dc14f0
chenky5354 <chen.ky@northeastern.edu>
2022-12-15 12:03:42 -0500

    implemented login and register

--- project/src/users/login.js
+++ project/src/users/login.js
@@ -1,12 +1,33 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {loginThunk} from "./users-thunk";
+
 const Login = () => {
+    const {currentUser} = useSelector((state) => state.users)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const handleLoginBtn = () => {
+        try {
+            dispatch(loginThunk({username, password}))
+            // navigate('/profile')
+        } catch (e) {
+
+        }
+        if (currentUser) {
+            return (<Navigate to={'/profile'}/>)
+        }
+    }
     return (
         <>
             <h1 className="text-center">Login here</h1>
-            <input className="form-control border border-primary w-50 offset-3"
-                   placeholder="Username" />
-            <input className="mt-3 form-control border border-primary w-50 offset-3"
-                   placeholder="Password" />
-            <button className="btn btn-primary mt-3 offset-3">
+            <input onChange={(e) => setUsername(e.target.value)}
+                   className="form-control border border-primary w-50 offset-3"
+                   placeholder="Username" value={username}/>
+            <input onChange={(e) => setPassword(e.target.value)}
+                   className="mt-3 form-control border border-primary w-50 offset-3"
+                   placeholder="Password" value={password}/>
+            <button className="btn btn-primary mt-3 offset-3" onClick={handleLoginBtn}>
                 Login
             </button>
         </>
